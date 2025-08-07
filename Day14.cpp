6416ec662230416226737284e3a0630a1a9b27cc
Avi Jain <157308574+awheee@users.noreply.github.com>
2025-08-07 23:29:27 +0530

    Update Day14.cpp

--- Day14.cpp
+++ Day14.cpp
@@ -6,12 +6,9 @@ using namespace std;
 int main() {
     string inp;
     cin >> inp;
-
-    // Capitalize the first character if it's lowercase
     if (islower(inp[0])) {
         inp[0] = toupper(inp[0]);
     }
-
     cout << inp << endl;
     return 0;
 }
