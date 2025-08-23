23eae234f78b452be24d79d8996655bcf3df7e9c
Avi Jain <157308574+awheee@users.noreply.github.com>
2025-08-23 23:57:09 +0530

    Update Day30.cpp

--- Day30.cpp
+++ Day30.cpp
@@ -2,20 +2,16 @@
 using namespace std;
 
 int main() {
-    int n;
-    cin >> n;
-    string s;
-    cin >> s;
+    int a, b;
+    cin >> a >> b;
 
-    int a = 0, d = 0;
-    for (char c : s) {
-        if (c == 'A') a++;
-        else d++;
+    int years = 0;
+    while (a <= b) {
+        a *= 3;
+        b *= 2;
+        years++;
     }
 
-    if (a > d) cout << "Anton" << endl;
-    else if (d > a) cout << "Danik" << endl;
-    else cout << "Friendship" << endl;
-
+    cout << years << endl;
     return 0;
 }
