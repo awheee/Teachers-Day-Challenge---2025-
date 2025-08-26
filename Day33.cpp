3ce0908e582c7b2a53f4677fc88cbb8d42e18479
Avi Jain <157308574+awheee@users.noreply.github.com>
2025-08-27 00:43:52 +0530

    Create Day33.cpp

---
+++ Day33.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string a, b;
+    cin >> a >> b;
+    for (int i = 0; i < a.size(); i++) {
+        cout << (a[i] == b[i] ? '0' : '1');
+    }
+    return 0;
+}
